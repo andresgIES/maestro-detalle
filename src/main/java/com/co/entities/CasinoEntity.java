0b1f79a466aa60561a7026352fccfe8c3cfade29
andresgIES <andres.garcia@iesonline.com.co>
2021-01-26 11:10:25 -0500

    feat: se añaden anotaciones al resto de campos (CasinoEntity.java)

--- src/main/java/com/co/entities/CasinoEntity.java
+++ src/main/java/com/co/entities/CasinoEntity.java
@@ -1,8 +1,12 @@
 package com.co.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -12,9 +16,19 @@ public class CasinoEntity {
 	@Id
 	@Column(name = "idCasino", nullable = false, unique = true)
 	private Integer id;
+	
+	@Column(name = "nombre", nullable = false)
 	private String nombre;
+	
+	@Column(name = "direccion", nullable = false)
 	private String direccion;
+	
+	@Column(name = "cantidad_maquinas", nullable = false)
 	private Integer cantidadMaquinas;
+	
+	@OneToOne(cascade = { CascadeType.REMOVE, CascadeType.DETACH,
+			CascadeType.REFRESH }, fetch = FetchType.LAZY)
+	@JoinColumn(name = "id", nullable = false)
 	private MaquinaEntity maquina;
 
 
