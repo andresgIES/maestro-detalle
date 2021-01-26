af2bdad19ce8bc2ff6a5a53c0728c21ddef7e8d9
andresgIES <andres.garcia@iesonline.com.co>
2021-01-26 15:24:26 -0500

    feat: se añaden metodos faltantes del crud (ClienteRestController.java)

--- src/main/java/com/co/restcontrollers/ClienteRestController.java
+++ src/main/java/com/co/restcontrollers/ClienteRestController.java
@@ -3,7 +3,10 @@ package com.co.restcontrollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,24 @@ public class ClienteRestController {
 	public ResponseEntity<Integer> consultar() {
 		serviceClientes.listAll();
 		return ResponseEntity.status(HttpStatus.OK).body(serviceClientes.listAll().size());
+	}
+	
+	@PostMapping("/crear")
+	public ResponseEntity<Integer> insertar() {
+		serviceClientes.save(null);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	@PutMapping("/actualizar")
+	public ResponseEntity<Integer> actualizar() {
+		serviceClientes.save(null);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	@DeleteMapping("/eliminar")
+	public ResponseEntity<Integer> borrar() {
+		serviceClientes.save(null);
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
 }
