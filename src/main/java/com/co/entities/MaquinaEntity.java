5922ec5cd0da8c2cb1a3aee361dbd1632623335d
andresgIES <andres.garcia@iesonline.com.co>
2021-01-26 11:07:42 -0500

    feat: se añaden anotaciones al resto de campos (MaquinaEntity.java)

--- src/main/java/com/co/entities/MaquinaEntity.java
+++ src/main/java/com/co/entities/MaquinaEntity.java
@@ -12,8 +12,14 @@ public class MaquinaEntity {
 	@Id
 	@Column(name = "idMaquina", nullable = false, unique = true)
 	private Integer id;
+	
+	@Column(name = "modelo", nullable = false)
 	private String modelo;
+	
+	@Column(name = "cantidad_juegos", nullable = false)
 	private Integer cantidadJuegos;
+	
+	@Column(name = "pantalla_tactil", nullable = false)
 	private boolean pantallaTactil;
 
 }
