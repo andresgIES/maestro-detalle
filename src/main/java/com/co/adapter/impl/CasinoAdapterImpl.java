44605ebb3a479ea30e45e772ec5398d7ba3e6b0f
andresgIES <andres.garcia@iesonline.com.co>
2021-01-26 14:06:44 -0500

    feat: se añade el mapper y se completan los metodos (CasinoAdapterImpl.java)

--- src/main/java/com/co/adapter/impl/CasinoAdapterImpl.java
+++ src/main/java/com/co/adapter/impl/CasinoAdapterImpl.java
@@ -1,21 +1,23 @@
 package com.co.adapter.impl;
 
+import org.modelmapper.ModelMapper;
+
 import com.co.adapter.interfaces.Adapter;
 import com.co.domain.Casino;
 import com.co.entities.CasinoEntity;
 
 public class CasinoAdapterImpl implements Adapter<Casino, CasinoEntity> {
+	
+	private ModelMapper mapper = new ModelMapper();
 
 	@Override
 	public Casino convertTo(CasinoEntity e) {
-		// TODO Auto-generated method stub
-		return null;
+		return (e == null) ? null : mapper.map(e, Casino.class);
 	}
 
 	@Override
 	public CasinoEntity convertFrom(Casino d) {
-		// TODO Auto-generated method stub
-		return null;
+		return (d == null ) ? null : mapper.map(d, CasinoEntity.class);
 	}
 
 }
